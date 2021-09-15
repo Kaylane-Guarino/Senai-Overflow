bcbef96c24d4dce0a42927d2ac923c9d54056ca7
Kaylane-Guarino <guarinokaah@gmail.com>
2021-09-15 11:28:34 -0300

    15/09

---
+++ senai-overflow-api/src/routes.js
@@ -0,0 +1,19 @@
+const routes = require("express").Router();
+
+const postController = require("./controllers/posts");
+const sessionController = require("./controllers/sessions");
+const userController = require("./controllers/users");
+const authMiddleware = require("./middlewares/auth");
+const uploadSingleImage = require("./middlewares/uploadSingleImage");
+
+//rotas públicas
+routes.post('/sessions', sessionController.store);
+routes.post('/users', userController.store);
+
+routes.use(authMiddleware);
+
+//rotas privadas
+routes.get('/posts', postController.index);
+routes.post('/posts', uploadSingleImage, postController.store);
+
+module.exports = routes;
